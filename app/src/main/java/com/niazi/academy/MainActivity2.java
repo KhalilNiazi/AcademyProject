70faf67d7203dc81c2a46549c83e8c7650c9c732
KhalilNiazi <khalilakbar703@gmail.com>
2023-09-07 00:05:20 +0500

    textview add kya

--- app/src/main/java/com/niazi/academy/MainActivity2.java
+++ app/src/main/java/com/niazi/academy/MainActivity2.java
@@ -17,12 +17,12 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
 
-     /*   String name = getIntent().getStringExtra("name");
+      String name = getIntent().getStringExtra("name");
         String email = getIntent().getStringExtra("email");
         String phone = getIntent().getStringExtra("phoneno");
         String address = getIntent().getStringExtra("address");
         int image = getIntent().getIntExtra("image",0);
-
+/*
         binding.editText.setText(name);
         binding.email.setText(email);
         binding.editTextPhone2.setText(phone);
